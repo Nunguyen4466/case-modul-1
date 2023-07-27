231efaeb48628bba98982c28d3c53102510c169e
Nunguyen4466 <nu3793@gmail.com>
2023-07-28 00:18:29 +0900

    update

--- kethuc/sanpham.js
+++
@@ -1,8 +0,0 @@
-let dropbtn = document.querySelector('.dropbtn');
-console.log(dropbtn);
-let dropshow = document.querySelector('.drop-content');
-const nut = (e) => {
-    console.log('sá');
-    dropshow.classList.toggle('show');
-};
-dropbtn.addEventListener('click', nut);
--- sanpham.js
+++ sanpham.js
@@ -1,8 +1,8 @@
 let dropbtn = document.querySelector('.dropbtn');
-console.log(dropbtn);
+
 let dropshow = document.querySelector('.drop-content');
 const nut = (e) => {
-    console.log('sá');
+  
     dropshow.classList.toggle('show');
 };
 dropbtn.addEventListener('click', nut);
